9082f33db40ccf99395832f55598aecde2e86a3f
lucas <lffsantos@gmail.com>
2016-01-16 03:12:06 -0300

    enable admin

--- car_location/urls.py
+++ car_location/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 
 from car_location.location.routers import router
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^api/v1/', include(router.urls, namespace='location')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
